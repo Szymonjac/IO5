23125cc3a65ff63c2da22901ec67ef579e00fc61
Szymon Jackiewicz <szymon@jackiewi.cz>
2022-02-20 20:44:19 +0100

    Add main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-
+#include "user.h"
 int main(){
-
-
+    user logedPerson;
+    std::string login, password;
+    std::cout<<"Witamy w programi super kolejki"<<std::endl;
+    std::cout<<"Login:";
+    std::cin>>login;
+    std::cout<<"Password:";
+    std::cin>>password;
+    logedPerson.login(login, password);
     return 0;
 }
